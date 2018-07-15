82e0339a230a4eec2f49479025c40b91a23f7d28
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-07-15 10:20:14 +0200

    Define the action creators

---
+++ src/js/redux/actionCreators.js
@@ -0,0 +1,77 @@
+import {
+  ADD_PRODUCT,
+  EDIT_PRODUCT,
+  DELETE_PRODUCT,
+  CHANGE_CATEGORY_FILTER,
+  CHANGE_NAME_FILTER,
+  CHANGE_SORTING,
+  ADD_COMMENT
+} from './actionTypes';
+
+const addProduct = (name, photo, category, prices, description) => {
+  return {
+    type: ADD_PRODUCT,
+    name,
+    photo,
+    category,
+    prices,
+    description
+  };
+};
+
+const editProduct = (index, name, photo, category, prices, description) => {
+  return {
+    type: EDIT_PRODUCT,
+    index,
+    name,
+    photo,
+    category,
+    prices,
+    description
+  };
+};
+
+const deleteProduct = index => {
+  return {
+    type: DELETE_PRODUCT,
+    index
+  };
+};
+
+const changeCategoryFilter = filter => {
+  return {
+    type: CHANGE_CATEGORY_FILTER,
+    filter
+  };
+};
+
+const changeNameFilter = filter => {
+  return {
+    type: CHANGE_NAME_FILTER,
+    filter
+  };
+};
+
+const changeSorting = sorting => {
+  return {
+    type: CHANGE_SORTING,
+    sorting
+  };
+};
+
+const addComment = (index, comment) => {
+  return {
+    type: ADD_COMMENT,
+    comment
+  };
+};
+
+export {
+  addProduct,
+  editProduct,
+  deleteProduct,
+  changeCategoryFilter,
+  changeNameFilter,
+  changeSorting,
+  addComment
+};
